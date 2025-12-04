b066520eb210d4158e9ada7c45a4244025f4d3c2
Karol Nowacki <karol.nowacki@scylladb.com>
2025-12-04 11:53:10 +0100

    vector-store: Fix usearch simulator crash
    The simulator crashes if a search was performed on an empty index
    (i.e. before any keys were indexed).

--- crates/vector-store/src/index/usearch.rs
+++ crates/vector-store/src/index/usearch.rs
@@ -323,14 +323,22 @@ impl UsearchIndex for RwLock<Simulator> {
         let start = Instant::now();
 
         let sim = self.read().unwrap();
-        let len = sim.keys.read().unwrap().len() as u64;
-        let keys: Vec<_> = iter::repeat_with(|| rand::random_range(0..len))
-            .map(Key)
-            .filter(|key| sim.keys.read().unwrap().contains(key))
-            .take(limit.0.get())
-            .collect();
-
         sim.wait_search(start);
+
+        let keys = {
+            let keys = sim.keys.read().unwrap();
+            let len = keys.len() as u64;
+            if len == 0 {
+                Vec::new()
+            } else {
+                iter::repeat_with(|| rand::random_range(0..len))
+                    .map(Key)
+                    .filter(|key| keys.contains(key))
+                    .take(limit.0.get())
+                    .collect()
+            }
+        };
+
         Ok(keys.into_iter().map(|key| (key, 0.0.into())))
     }
 
