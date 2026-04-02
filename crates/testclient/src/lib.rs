4b4e2b54c2fdef79da494458aa2c2e3cd23a369f
Pawel Pery <pawel.pery@scylladb.com>
2026-04-02 16:25:41 +0200

    testclient: implement testclient
    The testclient crate will be used for benches test - instead of request ann
    search via http io this crate uses axum-test to directly call http server's
    Router.

---
+++ crates/testclient/src/lib.rs
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2026-present ScyllaDB
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.1
+ */
+
+use axum::Router;
+use axum_test::TestResponse;
+use axum_test::TestServer;
+use httpapi::IndexName;
+use httpapi::KeyspaceName;
+use httpapi::Limit;
+use httpapi::PostIndexAnnFilter;
+use httpapi::PostIndexAnnRequest;
+use httpapi::Vector;
+
+#[derive(Debug)]
+pub struct TestClient {
+    test_server: TestServer,
+    url_api: String,
+}
+
+impl TestClient {
+    pub fn new(router: Router) -> Self {
+        Self {
+            url_api: "/api/v1".to_string(),
+            test_server: TestServer::new(router),
+        }
+    }
+
+    pub async fn index_status(
+        &self,
+        keyspace_name: &KeyspaceName,
+        index_name: &IndexName,
+    ) -> TestResponse {
+        self.test_server
+            .get(&format!(
+                "{}/indexes/{}/{}/status",
+                self.url_api, keyspace_name, index_name
+            ))
+            .await
+    }
+
+    pub async fn ann(
+        &self,
+        keyspace_name: &KeyspaceName,
+        index_name: &IndexName,
+        vector: Vector,
+        filter: Option<PostIndexAnnFilter>,
+        limit: Limit,
+    ) -> TestResponse {
+        self.test_server
+            .post(&format!(
+                "{}/indexes/{}/{}/ann",
+                self.url_api, keyspace_name, index_name
+            ))
+            .json(&PostIndexAnnRequest {
+                vector,
+                filter,
+                limit,
+            })
+            .await
+    }
+}
